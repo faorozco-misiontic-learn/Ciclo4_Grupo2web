eb0390ab2728bd3494ec9062a54898c75b6b71de
faorozco <faorozco@utp.edu.co>
2021-11-13 00:17:47 -0500

    integrate back and empleados_front

---
+++ back/models/empleados.models.js
@@ -0,0 +1,14 @@
+/* Vamos a crear un esquema de los registros de la base de datos*/
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EmpleadosSchema = new Schema({
+  nombre: { type: String, required: true, max: 60 },
+  apellido_p: { type: String, required: true, max: 40 },
+  apellido_m: { type: String, required: true, max: 40 },
+  telefono: { type: String, required: true, max: 15 },
+  mail: { type: String, required: false, max: 80 },
+  direccion: { type: String, required: false, max: 60 },
+});
+
+module.exports = mongoose.model("empleados", EmpleadosSchema);
---
+++ empleados_front/src/components/Login/Login.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuario: "",
+      pass: "",
+    };
+  }
+  render() {
+    return (
+      <Container id="login-container" style={{ marginTop: 200 }}>
+        <Row>
+          <Col>
+            <Row>
+              <h2>Iniciar Sesión</h2>
+            </Row>
+            <Row>
+              <Col
+                sm="12"
+                xs="12"
+                md={{ span: 4, offset: 4 }}
+                lg={{ span: 4, offset: 4 }}
+                xl={{ span: 4, offset: 4 }}
+              >
+                <Form>
+                  <Form.Group>
+                    <Form.Label style={{ float: "left" }}>Usuario</Form.Label>
+                    <Form.Control
+                      onChange={(e) =>
+                        this.setState({ usuario: e.target.value })
+                      }
+                    />
+                    {/* {this.state.usuario} */}
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label style={{ float: "left" }}>
+                      Contraseña
+                    </Form.Label>
+                    <Form.Control
+                      type="password"
+                      onChange={(e) => this.setState({ pass: e.target.value })}
+                    />
+                    {/* {this.state.pass} */}
+                  </Form.Group>
+
+                  <Button
+                    variant="primary"
+                    style={{
+                      marginTop: 20,
+                      width: "100%",
+                    }}
+                  >
+                    Iniciar Sesión
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
